7804adc209cc5e6a2f54c55ac090cf35e8b0c41f
natalia <natalia@nataliae-VirtualBox>
2019-10-16 22:42:26 -0400

    added some comments

--- src/main/java/edu/uprm/cse/datastructures/cardealer/util/CircularSortedDoublyLinkedList.java
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/util/CircularSortedDoublyLinkedList.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 
-
+	/**
+	 * Additional sub class for the Node
+	 */
 	private class Node<E>{
 		private E element;
 		private Node<E> next, prev;
@@ -42,7 +44,11 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 	}
 	
 	
-	
+	/**
+	 * Creates an Iterator
+	 * 
+	 * @return the Node Element - if not found returns null, else returns the element.
+	 */
 	private class CircularDoublyLinkedListIterator<E> implements Iterator<E>{
 		Node<E> temp = new Node<E>();
 		@Override
@@ -58,28 +64,6 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 		}
 	}
 	
-	/*
-	private class CircularDoublyLinkedListReverseIterator<E> implements Iterator<E>{
-		Node<E> temp = new Node<E>();
-		
-		@Override
-		public boolean hasNext() {
-			return temp.getPrev().getElement() != null;
-		}
-		
-		@Override
-		public E next() {
-			if(this.hasNext()) {
-				return temp.getPrev().getElement();
-			}
-			return null;
-		}
-	}
-	*/
-
-	
-	
-	
 	private int currentSize;
 	private Node<E> head;
 	private Comparator<E> comp;
@@ -93,7 +77,11 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 		comp = comparator;
 	}
 
-
+	/**
+	 * Returns an Iterator
+	 * 
+	 * @return Iterator - if the node isn't the head then return the iterator.
+	 */
 	@Override
 	public Iterator<E> iterator() {
 		List<E> newList = new ArrayList<E>();
@@ -107,28 +95,16 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 		return newList.iterator();
 	}
 	
-	/*
-	public Iterator<E> reverseIterator(){
-		List<E> newList = new ArrayList<E>();
-		
-		Node<E> newNode = this.head.getPrev();
-		
-		while(newNode != head) {
-			newList.add(newNode.getElement());
-			newNode = newNode.getPrev();
-		}
-		return newList.iterator();
-		
-	}
-	*/
-	
-	
-	
-	
+	/**
+	 * Adds a new node either at the beginning, end and in the middle.
+	 * 
+	 * @param obj - a node element.
+	 * @return true - if the insertion of the node was possible, else go to the next node.
+	 */
 	@Override
-	public boolean add(E obj) {
+	public boolean add(E e) {
 		if(this.isEmpty()){
-			Node<E> newNode = new Node<E>(obj, this.head, this.head);
+			Node<E> newNode = new Node<E>(e, this.head, this.head);
 			this.head.setNext(newNode);
 			this.head.setPrev(newNode);
 			currentSize++;
@@ -137,8 +113,8 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 		else{
 			Node<E> temp = head.getNext();
 			while(temp.getElement()!=null) {
-				if(this.comp.compare(obj, temp.getElement())<=0) {
-					Node<E> newNode = new Node<E>(obj, temp, temp.getPrev());
+				if(this.comp.compare(e, temp.getElement())<=0) {
+					Node<E> newNode = new Node<E>(e, temp, temp.getPrev());
 					temp.getPrev().setNext(newNode);
 					temp.setPrev(newNode);
 					currentSize++;
@@ -146,7 +122,7 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 				}
 				temp=temp.getNext();
 			}
-			Node<E> newNode = new Node<E>(obj, this.head, this.head.getPrev());
+			Node<E> newNode = new Node<E>(e, this.head, this.head.getPrev());
 			this.head.getPrev().setNext(newNode);
 			this.head.setPrev(newNode);
 			currentSize++;
@@ -159,6 +135,12 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 		return currentSize;
 	}
 
+	/**
+	 * Removes the element in a node.
+	 * 
+	 * @param e - node element.
+	 * @return false - if element is not there return false, else returns true.
+	 */
 	@Override
 	public boolean remove(E e) {
 		int i = this.firstIndex(e);
@@ -169,7 +151,13 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 		return false;
 		
 	}
-
+	
+	/**
+	 * removes a node at a specific index.
+	 * 
+	 * @param index - node index.
+	 * @return true - if invalid index returns IndexOutOfBoundsException, else returns true.
+	 */
 	@Override
 	public boolean remove(int index) {
 		if(index > this.size() || index < -1) {
@@ -195,25 +183,47 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 		}
 	}
 
+	/**
+	 * counts how many of the same element removed exists.
+	 * 
+	 * @param e - node element.
+	 * @return int - if not found keeps counting, else returns an int.
+	 */
 	@Override
-	public int removeAll(E obj) {
+	public int removeAll(E e) {
 		int count = 0;
-		while(this.remove(obj) != false) {
+		while(this.remove(e) != false) {
 			count++;
 		}
 		return count;
 	}
-
+	
+	/**
+	 * Gets the first element in the list.
+	 * 
+	 * @return element - if empty returns null, else returns the element.
+	 */
 	@Override
 	public E first() {
 		return (this.isEmpty() ? null : this.head.getNext().getElement());
 	}
 
+	/**
+	 * Gets the last element in the list.
+	 * 
+	 * @return element - if empty returns null, else returns the element.
+	 */
 	@Override
 	public E last() {
 		return (this.isEmpty() ? null : this.head.getPrev().getElement());
 	}
 
+	/**
+	 * Gets the element at a specific index.
+	 * 
+	 * @param index - node index.
+	 * @return element - if not reached end of list go to the next one, else returns the element.
+	 */
 	@Override
 	public E get(int index) {
 		Node<E> temp = this.head.getNext();
@@ -233,6 +243,12 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 
 	}
 
+	/**
+	 * tells you if the element exists.
+	 * 
+	 * @param e - node element.
+	 * @return true - if not found returns false, else returns true.
+	 */
 	@Override
 	public boolean contains(E e) {
 		if(this.firstIndex(e) != -1) {
@@ -246,6 +262,12 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 		return this.size() == 0;
 	}
 
+	/**
+	 * Gets the first index of a node element.
+	 * 
+	 * @param e - node element.
+	 * @return int - if not found returns -1, else returns the position i found.
+	 */
 	@Override
 	public int firstIndex(E e) {
 		int i = 0;
@@ -259,6 +281,12 @@ public class CircularSortedDoublyLinkedList<E> implements SortedList<E>{
 		return -1;
 	}
 
+	/**
+	 * Gets the last index of a node element.
+	 * 
+	 * @param e - node element.
+	 * @return int - if not found returns -1, else returns the position i found.
+	 */
 	@Override
 	public int lastIndex(E e) {
 		int i = 0, result = -1;
